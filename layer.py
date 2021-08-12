999f7a5e151f1fc103a84cb5aff98ad05e81f443
Oscar <s110616038@stu.ntue.edu.tw>
2021-08-13 01:56:30 +0800

    [Add] Add encoder

--- layer.py
+++ layer.py
@@ -4,18 +4,26 @@ import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #Encoder
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, latent_size):
+        #hidden_size is included Hidden len + Condition code len
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
 
         self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size)
+
+        self.linear_mean = nn.Linear(hidden_size, latent_size)
+        self.linear_logVar = nn.Linear(hidden_size, latent_size)
 
     def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
-        return output, hidden
+        #hidden must the tuple :(h_0, cell_0)
+        embedded = self.embedding(input)
+        for wordVec in embedded:
+            resizeWordVec = wordVec.view(1, 1, -1)
+            output, hidden = self.lstm(resizeWordVec, hidden)  #output is unuseful
+        mean = self.linear_mean(hidden)
+        logVar = self.linear_logVar(hidden)
+        return mean, logVar
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
